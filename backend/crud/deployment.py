10b407a69e0a56dad3132543712e56af5a1e183d
Morax19 <danielhdez19@gmail.com>
2025-08-12 15:57:36 -0400

    Changes to be committed: 	modified:   backend/crud/deployment.py 	modified:   backend/crud/settings.py

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -59,7 +59,3 @@ STORAGES = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-print("DJANGO_SETTINGS_MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'), file=sys.stderr)
-print("ALLOWED_HOSTS:", ALLOWED_HOSTS, file=sys.stderr)
-print("WEBSITE_HOSTNAME:", os.environ.get('WEBSITE_HOSTNAME'), file=sys.stderr)
