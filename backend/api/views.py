a0ef5d577ec663ff350894bd2ec3167ea89ba3fa
Morax19 <danielhdez19@gmail.com>
2025-08-29 15:45:12 -0400

    Changes to be committed: 	modified:   backend/api/views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -698,13 +698,15 @@ def adminLogin(request):
 
             # Retrieve user information and hashed password in a single query
             sql = """
-                SELECT U.Password, R.Description, U.userID
-                FROM Warranty.Users U JOIN Warranty.Role R ON U.roleID = R.RoleID
+                SELECT U.Password, R.Description, U.userID, C.FirstName
+                FROM Warranty.Users U
+                JOIN Warranty.Role R ON U.roleID = R.RoleID
+                JOIN Warranty.Customer C ON C.ID = U.CustomerID
                 WHERE U.Users = ?;
             """
             cursor.execute(sql, (email_address,))
             user_data = cursor.fetchone()
-
+            
             # Check if user exists and if the password is correct
             if not user_data:
                 return JsonResponse({
@@ -715,7 +717,8 @@ def adminLogin(request):
             stored_password = user_data[0]
             user_role = user_data[1]
             user_id = user_data[2]
-
+            user_fname = user_data[3]
+            print(user_fname)
             # Verify the password
             if not password == stored_password:
                 return JsonResponse({
@@ -732,6 +735,7 @@ def adminLogin(request):
 
             payload = {
                 'user_id': user_id,
+                'user_first_name': user_fname,
                 'email_address': email_address,
                 'role': user_role,
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
@@ -750,6 +754,7 @@ def adminLogin(request):
             if connection:
                 connection.rollback()
 
+            print(db_error)
             return JsonResponse({
                 'error': f'A database error ocurred: {db_error}',
                 'warning': 'Ha ocurrido un error, inténtelo más tarde.'
